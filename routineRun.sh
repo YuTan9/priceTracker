7921cd247f440536690a61a5997fc72fdec91f32
YuTan9 <s110095@shsh.tw>
2021-06-15 17:21:06 +0800

    [Debug] cleanup

--- routineRun.sh
+++ routineRun.sh
@@ -8,6 +8,7 @@ if python priceTracker.py; then
 	git push origin routineRun
 	git checkout master
 	git merge routineRun
+	git push
 else
 	echo "MyProtein price tracker running on raspberry pi crashed." | mail -s "Price Tracker Bug" s110095@shsh.tw
 fi
