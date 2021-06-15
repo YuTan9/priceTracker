3b67a0fd1a62a2e9880e79f7ddc9247d4cb04792
YuTan9 <s110095@shsh.tw>
2021-06-15 17:02:12 +0800

    [Debug] fix pi auto push

--- routineRun.sh
+++ routineRun.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 cd /home/pi/priceTracker
 git pull
+git checkout routineRun
 if python priceTracker.py; then
-	git checkout -b routineRun
-	git add log.csv discount.png
+	git add log.csv
 	git commit -m "Price tracker auto update."
 	git push origin routineRun
 	git checkout master
@@ -11,3 +11,4 @@ if python priceTracker.py; then
 else
 	echo "MyProtein price tracker running on raspberry pi crashed." | mail -s "Price Tracker Bug" s110095@shsh.tw
 fi
+git checkout master
